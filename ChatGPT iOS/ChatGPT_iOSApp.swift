5630b8d5302a0b8d2587f4cec8ebe1b3eabc3f05
lucasurbain <lucasurbain68@protonmail.ch>
2023-02-24 09:54:32 +0100

    Initial Commit

---
+++ ChatGPT iOS/ChatGPT_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPT_iOSApp.swift
+//  ChatGPT iOS
+//
+//  Created by lucas urbain on 24.02.23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPT_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
